89d57914796a700aa4713a26a2a75ab4334dfa36
NubNini <jennythach@live.com.au>
2018-09-03 14:32:47 +1000

    initial set up for youtube UI

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "https://github.com/StephenGrider/ReduxSimpleStarter.git";
+// Create a new component. This component
+// should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return <div>Hi!</div>;
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and
+// put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector(".container"));

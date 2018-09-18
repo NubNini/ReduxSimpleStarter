fba4464f1a66e67b12aee40841d3e2bb73c778b7
NubNini <jennythach@live.com.au>
2018-09-18 13:12:42 +1000

    work

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SearchBar = () => {
-  return <input placeholder="Search" />;
-};
+// Class level components
+class SearchBar extends Component {
+  // Initialise a state in constructor
+  // Create constructor method and argument props
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+  render() {
+    // Functional Component
+    return <input placeholder="Search" onChange={this.onInputChange} />;
+  }
+
+  // Arrow Function
+  onInputChange = e => {
+    console.log(e.target.value);
+  };
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
 
-const API_KEY = "";
+const API_KEY = "AIzaSyDDVFUvxhATcowSiFQtTxLz77opviEBP14";
 // Create a new component. This component
 // should produce some HTML
 
